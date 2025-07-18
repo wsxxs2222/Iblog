db6e2dee35ca6533dea433e1ec9a77a9fb5f196f
qizhou <wsxxs2222@gmail.com>
2025-07-18 12:45:17 -0500

    fix the mismatch of post id in profile page and home page by filtering posts after they receive consistent ids

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,17 +5,15 @@ import { AppStateContext } from "../context";
 
 export default function ProfilePage() {
   const {postList, currentUser} = useContext(AppStateContext);
-  const currentUserPosts = postList.filter((post) => {
-      return post.username === currentUser.username;
+  const postComponentList = postList.map((post, index) => {
+      return <Post title={post.title} content={post.content} username={post.username} key={index} id={index}></Post>;
   });
   
   return <div>
     <h2>{currentUser?.username ?? ''}</h2>
     <h3>{currentUser?.accountCreatedTime.toISOString() ?? ''}</h3>
-    {currentUserPosts.map((post, index) => {
-      return <Post title={post.title} content={post.content} username={post.username} key={index} id={index}>
-
-      </Post>
+    {postComponentList.filter((postComponent) => {
+      return postComponent.props.username === currentUser?.username;
     })}
   </div>;
 }
