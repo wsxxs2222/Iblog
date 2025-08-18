9c31adc98b07cb40a1353e0b398af3baf574517f
qizhou <wsxxs2222@gmail.com>
2025-08-18 11:37:45 -0500

    allow create post for authed users only, show error message for unauthed users

--- app/lib/components/post_components/post_list_header.jsx
+++ app/lib/components/post_components/post_list_header.jsx
@@ -1,39 +1,50 @@
 'use client'
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { CreatePostModal } from "./create_post_area";
-import { useSession } from "next-auth/react";
+import { AppStateContext } from "../app_context";
 import '../../../ui/post_creation.css';
 import '../../../ui/component.css'
+import '../../../ui/global.css';
 
 export function PostListHeader() {
-    const session = useSession();
+    const {isLoggedIn} = useContext(AppStateContext);
     const [showPostModal, setShowPostModal] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
 
-    return <div id="post-list-header-row">
-        <button className="button-large primary"
-        onClick={
-            () => {
-                    if (session) {
-                        setShowPostModal(true);
+    return <div id="post-header-and-error-container">
+        <div id="post-list-header-row">
+            <button className="button-large primary"
+            onClick={
+                () => {
+                        if (isLoggedIn()) {
+                            setShowPostModal(true);
+                        } else {
+                            setErrorMessage("Please log in to make a post");
+                            setTimeout(() => {
+                                setErrorMessage(null);
+                            }, 5000);
+                        }
                     }
-                    // TODO: show error if not signed in
-                }
-        }
-        >Create Post</button>
-        <div id="sort-post-row">
-            <h2>Sort By:</h2>
-            <select name="sort-post-dropdown" id="sort-post-input">
-                <option value="most-recent">Most Recent</option>
-                <option value="most-liked">Most Liked</option>
-            </select>
+            }
+            >Create Post</button>
+            <div id="sort-post-row">
+                <h2>Sort By:</h2>
+                <select name="sort-post-dropdown" id="sort-post-input">
+                    <option value="most-recent">Most Recent</option>
+                    <option value="most-liked">Most Liked</option>
+                </select>
+            </div>
+            {showPostModal 
+                ? <CreatePostModal 
+                    onClose={() => {
+                        setShowPostModal(false);
+                    }}
+                ></CreatePostModal>
+                : null
+            }
         </div>
-        {showPostModal 
-            ? <CreatePostModal 
-                onClose={() => {
-                    setShowPostModal(false);
-                }}
-            ></CreatePostModal>
-            : null
-        }
+        {errorMessage 
+            ? <p className="error-message">{errorMessage}</p>
+            : null}
     </div>;
 }
